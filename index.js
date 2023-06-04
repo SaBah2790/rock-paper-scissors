b7d65c2b2fc90e05596932091c801e586166a3d4
SaBah2790 <sandra.bahmat@yahoo.com>
2023-06-04 18:54:38 +0200

    updated default choice

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //give the computer 3 choices and make it pick one
+
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   let random = Math.floor(Math.random() * 3);
   return choices[random];
 }
 //pick an option from the three and play against the computer
+
 function playGame(playerSelection, computerSelection) {
   playerSelection = prompt("So what will it be? Rock, paper or scissors?");
   let playersPick =
@@ -19,8 +21,9 @@ function playGame(playerSelection, computerSelection) {
     `Rock beats ${computerSelection}`
   );
 }
-const playerSelection = "rock";
+const playerSelection = "paper";
 computerSelection = getComputerChoice();
 console.log(playGame(playerSelection, computerSelection));
+
 //set-up the result with winning and losing combinations
 //declare winner
